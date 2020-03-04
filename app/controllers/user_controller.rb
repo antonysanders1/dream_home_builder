ad6ed6da408a10a3a4eb7d417644bd2f363f16f8
antony_sanders <antony.devon92@gmail.com>
2020-03-04 16:31:54 -0500

    Corrected css alignments and positions for .wrapper, logo, user header, and logout button

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,20 @@
 require './config/environment'
 
 class UserController < ApplicationController
+
+    post'/signup' do
+        @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+        if @user.save && @user.name != "" && @user.email != "" && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{params[:id]}"
+        else
+            redirect '/welcome'
+        end
+      end
+
+      get '/users/:id' do
+        @user = User.find_by(id: params[:id])
+        erb :"users/show"
+      end
+
 end
