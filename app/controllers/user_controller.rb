5adca7877f44aeb6b32e697279a056c02e70e9b6
antony_sanders <antony.devon92@gmail.com>
2020-03-05 13:12:50 -0500

    Authentication complete. Login functionalliy added. User can now Signup, Login, and logout. Session hold and clear data.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,6 +25,16 @@ class UserController < ApplicationController
         erb :'users/login'
     end
 
+    post '/login' do
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/home'
+      else
+        redirect '/login'
+      end
+    end
+
     get '/logout' do
         if logged_in?
           session.clear
